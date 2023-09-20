6d29518ad3d34734055101a242ea5fefeb5d0314
Gaurav Singh <gauravrajputsingh94@gmail.com>
2023-09-20 15:42:40 +0530

    🚀 Completed the full-stack connecting website.

---
+++ src/fetch/fetchData.ts
@@ -0,0 +1,59 @@
+import axios from "axios";
+
+export const sentNameData = async (name:string) => {
+    try {
+      const token = localStorage.getItem("token");
+      const body = { name };
+      const response = await axios.put(
+        "/api/add/updatename",
+        body,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  export const sentEmailData = async (email:string) => {
+    try {
+      const token = localStorage.getItem("token");
+      const body = { email };
+      const response = await axios.put(
+        "/api/add/updateemail",
+        body,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  export const sentPhoneData = async (phoneNo:string) => {
+    try {
+      const token = localStorage.getItem("token");
+      const body = { phoneNo };
+      const response = await axios.put(
+        "/api/add/updatephoneno",
+        body,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+    } catch (error) {
+      throw error;
+    }
+  };
+
